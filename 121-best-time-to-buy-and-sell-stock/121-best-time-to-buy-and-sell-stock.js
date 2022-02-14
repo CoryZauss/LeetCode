8c752ba29f56897397fc8e3f2ebb49ef2f9f28d4
Cory Zauss <coryclassic1989@gmail.com>
2022-02-14 15:53:13 -0500

    Time: 97 ms (76.21%), Space: 51.9 MB (23.85%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -3,31 +3,47 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    // assume array always populated >0/ array is always positive number
+//     // assume array always populated >0/ array is always positive number
+//     //init vals to track low and high and bigest diff
+//     //loop thru prices
+//     // if a num is lower than lowest 
+//       // loop thru rest of array for highest
+//       // set diff to highest - lowest
+//     //BRUTE SOLUTION
     
-    //init vals to track low and high and bigest diff
-    //loop thru prices
-    // if a num is lower than lowest 
-      // loop thru rest of array for highest
-      // set diff to highest - lowest
-    //BRUTE SOLUTION
+//     let lowest;
+//     let highest;
+//     let diff = 0;
+//     for ( let i = 0; i < prices.length; i+= 1) {
+//         if (prices[i] < lowest || lowest === undefined) {
+//             lowest = prices[i];
+//             highest = prices[i];
+//             for ( let j = i+1; j < prices.length; j+=1){
+//                 if (prices[j] > highest) {
+//                     highest = prices[j]
+//                 }
+//                 if (highest - lowest > diff) {
+//                     diff = highest - lowest
+//                 }
+//             }
+//         }
+//     }
+//     return diff
     
-    let lowest;
-    let highest;
-    let diff = 0;
-    for ( let i = 0; i < prices.length; i+= 1) {
-        if (prices[i] < lowest || lowest === undefined) {
-            lowest = prices[i];
-            highest = prices[i];
-            for ( let j = i+1; j < prices.length; j+=1){
-                if (prices[j] > highest) {
-                    highest = prices[j]
-                }
-                if (highest - lowest > diff) {
-                    diff = highest - lowest
-                }
-            }
+    
+    //NEW SOLUTION
+    // use 2 pointers
+    let left = 0;
+    let right = 1;
+    let profit = 0;
+    while (right < prices.length) {
+        if (prices[left] < prices[right]) {
+            let diff = prices[right] - prices[left]
+            profit = Math.max(diff, profit)
+            } else {
+            left = right;
         }
+        right++
     }
-    return diff
+    return profit
 };
