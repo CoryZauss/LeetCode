1f0cbfb0d8d244667160d29453b0988a09f886ac
Cory Zauss <coryclassic1989@gmail.com>
2022-02-23 18:05:35 -0500

    Time: 83 ms (63.84%), Space: 42 MB (43.97%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,27 +3,37 @@
  * @return {boolean}
  */
 var isValid = function(s) {
- 
-    
-    if (s.length < 2) return false
-    if (s.length % 2 !== 0) return false
-    
-    let stack = []
-    
-    for(let i = 0; i < s.length; i++) {
-        
-        if (s[i] === "("){
-            stack.push(")")
-        }else if (s[i] === "["){
-            stack.push("]")
-        }else if(s[i] === "{"){
-            stack.push("}")
-        }else if (s[i] !== stack.pop()){
-            return false
-        }
+    // STACK?
+    //itterate and push values into stack as long as they are openings
+    //if not an opening, value at top of stack can be removed with closing pair
+    //if not the correct closing pair, return false
+    if (s.length % 2 !== 0) {
+      return false;
     }
-    
-    return stack.length === 0
+    let brackets = {
+        '(': ')',
+        '{': '}',
+        '[':']'
+    }
+    let openings = ['(','{','[']
+    let stack = [];
 
+      for (let i = 0; i < s.length; i++){
+          //if its a key push to stack
+        if (brackets.hasOwnProperty(s[i])){
+            stack.push(s[i])
+        } else if (brackets[stack[stack.length-1] ] === s[i]){
+           //if its not pushing we are removeing from stack
+           // must check value to be removed matchs  closing bracket
+            stack.pop()
+        } else {
+            return false;
+        }
+      }
+    if (stack.length === 0) {
+         return true;
+    } else {
+        return false;
+    }
    
 };
