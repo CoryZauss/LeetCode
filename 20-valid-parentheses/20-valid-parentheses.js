df664a2b99cd589ef1c4581da7e98367d3ce64e2
Cory Zauss <coryclassic1989@gmail.com>
2022-02-23 18:06:23 -0500

    Time: 72 ms (83.39%), Space: 42.6 MB (18.81%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -3,10 +3,6 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    // STACK?
-    //itterate and push values into stack as long as they are openings
-    //if not an opening, value at top of stack can be removed with closing pair
-    //if not the correct closing pair, return false
     if (s.length % 2 !== 0) {
       return false;
     }
@@ -17,14 +13,10 @@ var isValid = function(s) {
     }
     let openings = ['(','{','[']
     let stack = [];
-
       for (let i = 0; i < s.length; i++){
-          //if its a key push to stack
         if (brackets.hasOwnProperty(s[i])){
             stack.push(s[i])
         } else if (brackets[stack[stack.length-1] ] === s[i]){
-           //if its not pushing we are removeing from stack
-           // must check value to be removed matchs  closing bracket
             stack.pop()
         } else {
             return false;
