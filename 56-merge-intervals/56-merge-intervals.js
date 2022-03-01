4cad558cd44ab541397bf7ffa3d06ba320494047
Cory Zauss <coryclassic1989@gmail.com>
2022-03-01 15:28:15 -0500

    Time: 128 ms (75.45%), Space: 48.6 MB (54.63%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -3,13 +3,17 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
+    //sort array first by starting indexs
     intervals.sort((a,b) => a[0]-b[0]);
+    //initialize result with first interval
     let result = [intervals[0]]
     let index = 0;
+    //move thru intervals, and check if an interval overlaps with last tupple in results
     for (let i=1; i < intervals.length; i++){
         if ( intervals[i][0] <= result[index][1]) {
             result[index][1] = Math.max(result[index][1], intervals[i][1])
         } else {
+            //if they dont overlap then we can push it and safely move index up
             result.push(intervals[i]);
             index++;
         }
