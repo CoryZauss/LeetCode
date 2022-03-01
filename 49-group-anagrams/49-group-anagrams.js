87dc7f620a9ce75c9f7cdda2ba863fd89cb050a1
Cory Zauss <coryclassic1989@gmail.com>
2022-03-01 16:34:42 -0500

    Time: 148 ms (79.98%), Space: 52.9 MB (47.57%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -2,47 +2,62 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
-var groupAnagrams = function(strs) {
-    
-  const isAna = function(w1, w2) {
-      if (w1.length !== w2.length) {
-          return false
-      }
-      let word1 = {}
-      for (let i = 0; i < w1.length; i++) {
-          if (word1[w1[i]] === undefined) {
-              word1[w1[i]] = 1;
-          } else {
-              word1[w1[i]] ++;
-          }
-      }
+// var groupAnagrams = function(strs) {
+    //brute bad 5% solution
+//   const isAna = function(w1, w2) {
+//       if (w1.length !== w2.length) {
+//           return false
+//       }
+//       let word1 = {}
+//       for (let i = 0; i < w1.length; i++) {
+//           if (word1[w1[i]] === undefined) {
+//               word1[w1[i]] = 1;
+//           } else {
+//               word1[w1[i]] ++;
+//           }
+//       }
      
-    for (let j = 0; j < w2.length; j++) {
-        if (word1[w2[j]]){
-            word1[w2[j]]--
-        } else {
-            return false
-        }
-    }
-      return true;
-  }
-  //
-  let result = [[strs[0]]];
-    for (let i = 1; i < strs.length; i++) {
-        let current = strs[i];
-        let foundgroup = false;
-        for (let j = 0; j < result.length; j++) {
-            let currentres = result[j][0]
-            if (isAna(current, currentres)) {
-                result[j].push(current);
-                foundgroup = true;
-                break;
-            }
-        }
-        if (!foundgroup) {
-            result.push([current])
-        }
-    }
-  return result
+//     for (let j = 0; j < w2.length; j++) {
+//         if (word1[w2[j]]){
+//             word1[w2[j]]--
+//         } else {
+//             return false
+//         }
+//     }
+//       return true;
+//   }
+//   //
+//   let result = [[strs[0]]];
+//     for (let i = 1; i < strs.length; i++) {
+//         let current = strs[i];
+//         let foundgroup = false;
+//         for (let j = 0; j < result.length; j++) {
+//             let currentres = result[j][0]
+//             if (isAna(current, currentres)) {
+//                 result[j].push(current);
+//                 foundgroup = true;
+//                 break;
+//             }
+//         }
+//         if (!foundgroup) {
+//             result.push([current])
+//         }
+//     }
+//   return result
     
+// };
+
+var groupAnagrams = function(strs) {
+    const sortedStringsArr = strs.map((str) => str.split('').sort().join(''));
+    
+    const resultStrings = sortedStringsArr.reduce((acc, curr, index) => {
+        if(acc[curr] !== undefined) {
+            acc[curr] = [...acc[curr], strs[index]]
+        } else {
+            acc[curr] = [strs[index]]
+        }
+        return acc;
+    }, {});
+    
+    return Object.values(resultStrings);
 };
