ede136e0d1a7aae9d954b46783686d57d2a4e14a
Cory Zauss <coryclassic1989@gmail.com>
2022-03-04 11:20:12 -0500

    Time: 112 ms (55.89%), Space: 49.3 MB (52.45%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -6,18 +6,16 @@ var maxArea = function(height) {
     let sums = {};
     let l = 0;
     let r = height.length -1;
+    high = 0;
     while ( l !== r) {
-        sums[`${l}, ${r}`] = Math.min(height[l], height[r]) * (Math.abs((l+1) - (r+1)))
+        temp = Math.min(height[l], height[r]) * (Math.abs((l+1) - (r+1)))
+      if (temp > high) {
+        high = temp
+      }
         if ( height[l] < height[r]) {
            l++ ;
         } else {
             r--;
-        }
-    }
-    let high = 0;
-    for ( let key in sums) {
-        if (sums[key] > high){
-            high = sums[key]
         }
     }
     return high
