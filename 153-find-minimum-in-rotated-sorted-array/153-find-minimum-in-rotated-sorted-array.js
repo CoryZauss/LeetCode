ae8076f33f99deee7238334b0bdf440a61d29b79
Cory Zauss <coryclassic1989@gmail.com>
2022-03-10 09:11:02 -0500

    Time: 61 ms (90.74%), Space: 42 MB (48.18%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.js
@@ -2,42 +2,22 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var findMin = function(nums) {
-//     let p1 = 0;
-//     let p2 = nums.length -1;
-    
-//     while (p1 <= p2) {
-//           const mid = Math.floor((p2 + p2) / 2); 
-//         if(nums[mid] < nums[mid - 1]){ 
-//             return nums[mid];
-//         } else {
-//             if(nums[mid] > nums[0]) {              
-//                 p1 = mid + 1;
-//             } else {
-//                 p2 =  mid - 1; 
-//             }
-//         }
-//     }
-    
-//     return nums[0]; 
-// };
-
-
 var findMin = function(nums) {
-    let start = 0;
-    let end = nums.length-1
-
-    while (start < end) {
-        if (nums[start] < nums[end]) {
-            return nums[start]
-        }
-        let mid = Math.floor((end+start)/2)
-        if (nums[start] > nums[mid]) {
-            end = mid
-        }
-        else {
-            start = mid + 1     
+    let p1 = 0;
+    let p2 = nums.length -1;
+    
+    while (p1 <= p2) {
+          const mid = Math.floor((p2 + p2) / 2); 
+        if(nums[mid] < nums[mid - 1]){ 
+            return nums[mid];
+        } else {
+            if(nums[mid] > nums[0]) {              
+                p1 = mid + 1;
+            } else {
+                p2 =  mid - 1; 
+            }
         }
     }
-    return nums[start]
+    
+    return nums[0]; 
 };
