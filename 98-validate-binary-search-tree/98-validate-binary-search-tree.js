103f6ba3caf33a15de8d6e83a12fe0ea3281a0da
Cory Zauss <coryclassic1989@gmail.com>
2022-03-30 18:16:17 -0400

    Time: 64 ms (97.86%), Space: 46.4 MB (42.68%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.js
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.js
@@ -10,19 +10,29 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isValidBST = function(root) {
-   let stack = [], current = root, prev;
+// var isValidBST = function(root) {
+//    let stack = [], current = root, prev;
     
-    while ( current || stack.length) {
-        while ( current != null) {
-            stack.push(current)
-            current = current.left;
-        }
-        current = stack.pop();
-        if (prev && current.val <= prev.val) return false;
+//     while ( current || stack.length) {
+//         while ( current != null) {
+//             stack.push(current)
+//             current = current.left;
+//         }
+//         current = stack.pop();
+//         if (prev && current.val <= prev.val) return false;
         
-        prev = current
-        current = current.right
-    }
-    return true
-};
+//         prev = current
+//         current = current.right
+//     }
+//     return true
+// };
+
+const dfs = (node, l, r) => {
+    if (!node) return true;
+    if (l && l.val >= node.val) return false;
+    if (r && r.val <= node.val) return false;
+    return dfs(node.left, l, node) && dfs(node.right, node, r)
+}
+
+const isValidBST = (root) => dfs(root, null, null)
+
