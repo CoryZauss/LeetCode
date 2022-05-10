9373d1c0fe6e72dfe212207f5de4e64106804517
Cory Zauss <coryclassic1989@gmail.com>
2022-05-10 15:24:01 -0400

    Time: 136 ms (80.69%), Space: 46 MB (99.10%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -3,27 +3,34 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-      const symbols={
-        "I":1,
-        "V":5,
-        "X":10,
-        "L":50,
-        "C":100,
-        "D":500,
-        "M":1000
+     const list = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  let total = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    let currentInt = list[s.charAt(i)]; //Look at the first roman numeral pass it through the map and get the value
+    let nextInt = list[s.charAt(i + 1)];
+
+    if (nextInt) {
+      //Checks for presence of a nextInt (edge case)
+      if (currentInt >= nextInt) {
+        //If currentInt is greater than or equal to nextInt
+        total += currentInt; //Add the current integer to the total
+      } else {
+        total += nextInt - currentInt; //If currentInt is not greater than or equal to nextInt we subtract and add it to total
+        i++; //Since we have accounted for both values we are incrementing to check next set
+      }
+    } else {
+      total += currentInt;
     }
-    const characterList = s.split("");
-   
-    sum=0
-    characterList.map((data,i)=>{
-        const currentValue = symbols[characterList[i]];
-        const nextValue = characterList[i+1] ? symbols[characterList[i+1]]:0
-        if(currentValue<nextValue){
-            sum -=currentValue
-        }else{
-            sum +=currentValue
-        }
-        
-    });
-    return sum;
+  }
+  return total;
 };
