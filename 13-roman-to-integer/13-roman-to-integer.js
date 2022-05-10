773ef158ee2d7cd83a23b6bbbdac0be8b8562497
Cory Zauss <coryclassic1989@gmail.com>
2022-05-10 15:22:38 -0400

    Time: 280 ms (10.75%), Space: 48.5 MB (27.38%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.js
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -3,43 +3,27 @@
  * @return {number}
  */
 var romanToInt = function(s) {
-    let sum = 0;
-    let values = {
-        'I': 1,
-        'V': 5,
-        'X': 10,
-        'L': 50,
-        'C': 100,
-        'D': 500,
-        'M': 1000,
+      const symbols={
+        "I":1,
+        "V":5,
+        "X":10,
+        "L":50,
+        "C":100,
+        "D":500,
+        "M":1000
     }
-    for (let i = s.length-1; i >= 0; i--) {
+    const characterList = s.split("");
+   
+    sum=0
+    characterList.map((data,i)=>{
+        const currentValue = symbols[characterList[i]];
+        const nextValue = characterList[i+1] ? symbols[characterList[i+1]]:0
+        if(currentValue<nextValue){
+            sum -=currentValue
+        }else{
+            sum +=currentValue
+        }
         
-        if (s[i] === 'V' && s[i-1] === 'I') {
-            console.log('hi')
-            sum += 4;
-            i--;
-            } else if ( s[i] === 'X' && s[i-1] === 'I'){
-                sum += 9;
-                i--;
-            } else if ( s[i] === 'C' && s[i-1] === 'X') {
-                sum+= 90;
-                i--;
-            } else if ( s[i] === 'L' && s[i-1] === 'X') {
-                sum+= 40;
-                i--
-                           
-            } else if ( s[i] === 'M' && s[i-1] === 'C') {
-                sum+=900;
-                i--;
-                           
-            } else if ( s[i] === 'D' && s[i-1] === 'C') {
-                sum+=400;
-                i--;
-            } else {
-                sum += values[s[i]]   
-            }
-            console.log(s[i], sum)
-    }
+    });
     return sum;
 };
