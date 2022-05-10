c4626ba6d5e0bdeb183c5ee90418f31b2b718a92
Cory Zauss <coryclassic1989@gmail.com>
2022-05-10 13:47:37 -0400

    Time: 263 ms (42.54%), Space: 51.2 MB (52.42%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -3,18 +3,16 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-//     let str = x.toString()
-//     let p1 = 0, p2 = str.length-1;
+    let str = x.toString()
+    let p1 = 0, p2 = str.length-1;
     
-//     while (p1<p2){
-//         if (str[p1] !== str[p2]) {
-//             return false
-//         }
-//         p1++;
-//         p2--;
-//     }
+    while (p1<p2){
+        if (str[p1] !== str[p2]) {
+            return false
+        }
+        p1++;
+        p2--;
+    }
     
-//     return true
-    
-    return x.toString().split('').reverse().join('') == x.toString();
+    return true
 };
