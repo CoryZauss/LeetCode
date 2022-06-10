99eb9bc13feb066039bd4d7f79ff50afa79d37f9
Cory Zauss <coryclassic1989@gmail.com>
2022-06-10 14:21:35 -0400

    Time: 85 ms (71.05%), Space: 44.1 MB (84.66%) - LeetHub

--- 268-missing-number/268-missing-number.js
+++ 268-missing-number/268-missing-number.js
@@ -2,13 +2,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-var missingNumber = function(nums) {
+// var missingNumber = function(nums) {
   
-    for ( let i = 0; i < nums.length; i++) {
-        if (nums.indexOf(i) === -1) {
-            return i;
-        }
-    }
-    return nums.length;
+//     for ( let i = 0; i < nums.length; i++) {
+//         if (nums.indexOf(i) === -1) {
+//             return i;
+//         }
+//     }
+//     return nums.length;
     
+// };
+
+const missingNumber = function (nums) {
+	return (nums.length * (nums.length + 1)) / 2 - nums.reduce((a, b) => a + b, 0);
 };
