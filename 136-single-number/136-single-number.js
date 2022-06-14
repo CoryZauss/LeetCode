1460acb1bc1e22437c8258f236d392252690a7fa
Cory Zauss <coryclassic1989@gmail.com>
2022-06-14 18:56:50 -0400

    Time: 98 ms (57.54%), Space: 43.3 MB (81.40%) - LeetHub

--- 136-single-number/136-single-number.js
+++ 136-single-number/136-single-number.js
@@ -4,20 +4,21 @@
  */
 var singleNumber = function(nums) {
     // must be linear, no extra space ( in place)
-    let p1 = 1;
-        while( p1 < nums.length) {
-            if (nums[0] === nums[p1]) {
+//     let p1 = 1;
+//         while( p1 < nums.length) {
+//             if (nums[0] === nums[p1]) {
                 
-                nums.splice(p1, 1);
-                nums.splice(0, 1);
-                p1 = 0;
-            } else if (p1 === nums.length -1 && nums[0] !== nums[p1]) {
-                return nums[0]
-            }
+//                 nums.splice(p1, 1);
+//                 nums.splice(0, 1);
+//                 p1 = 0;
+//             } else if (p1 === nums.length -1 && nums[0] !== nums[p1]) {
+//                 return nums[0]
+//             }
             
-            p1++
+//             p1++
             
-        }
+//         }
     
-    return nums[0]
+//     return nums[0]
+    return nums.reduce((prev, curr) => prev ^ curr);
 };
