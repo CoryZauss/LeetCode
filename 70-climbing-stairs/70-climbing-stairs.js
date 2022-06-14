35d671e61e4ce787032bd74cee5984dfcb67844b
Cory Zauss <coryclassic1989@gmail.com>
2022-06-14 19:44:30 -0400

    Time: 81 ms (46.08%), Space: 42.4 MB (6.36%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -2,6 +2,24 @@
  * @param {number} n
  * @return {number}
  */
+// recursive ----
+// var climbStairs = function(n, memo = new Array()) {
+//     if (n === 1) {
+//         return 1;
+//     }
+//     if (n === 2) {
+//         return 2;
+//     }
+//     if (memo[n] !== undefined) {
+//         return memo[n];
+//     }
+//     let result = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+//     memo[n] = result;
+//     return result;
+    
+// };
+
+// dynamic programming ---
 var climbStairs = function(n, memo = new Array()) {
     if (n === 1) {
         return 1;
@@ -12,8 +30,9 @@ var climbStairs = function(n, memo = new Array()) {
     if (memo[n] !== undefined) {
         return memo[n];
     }
-    let result = climbStairs(n-1, memo) + climbStairs(n-2, memo);
-    memo[n] = result;
-    return result;
-    
+    let res = climbStairs(n-1, memo) + climbStairs(n-2, memo);
+    memo[n] = res;
+    return res;
+    // T.C: O(N)
+    // S.C: O(N)
 };
