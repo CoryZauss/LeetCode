133c861ac2c719f9b07c71ed9e9df74dbb21c9a8
Cory Zauss <coryclassic1989@gmail.com>
2022-06-24 12:35:12 -0400

    Time: 228 ms (40.65%), Space: 69.4 MB (81.45%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.js
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -9,20 +9,39 @@
  * @param {ListNode} head
  * @return {boolean}
  */
-var isPalindrome = function(head) {
-    let values = []
-    while (head !== null) {
-        values.push(head.val)
-        head = head.next
+// var isPalindrome = function(head) {
+//     let values = []
+//     while (head !== null) {
+//         values.push(head.val)
+//         head = head.next
+//     }
+//     let right = values.length-1
+//     let left = 0;
+//     while (left <right){
+//         if (values[left] !== values[right]) {
+//             return false
+//         }
+//         left++
+//         right--
+//     }
+//     return true
+// };
+
+var isPalindrome = function( head ) {
+    let fast = head, slow = head, prev, temp
+    while (fast && fast.next) {
+        slow = slow.next
+        fast = fast.next.next
     }
-    let right = values.length-1
-    let left = 0;
-    while (left <right){
-        if (values[left] !== values[right]) {
-            return false
-        }
-        left++
-        right--
+    prev = slow, slow = slow.next, prev.next = null;
+    
+    while (slow) {
+        temp = slow.next, slow.next = prev, prev = slow, slow = temp
+    }
+    fast = head, slow = prev;
+    while (slow) {
+        if (fast.val !== slow.val) return false
+        else fast = fast.next, slow = slow.next
     }
     return true
-};
+}
