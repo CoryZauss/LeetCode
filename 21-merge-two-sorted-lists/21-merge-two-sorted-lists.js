d47bfe9a66389ab8374a2d3110a6c4c898208e5b
Cory Zauss <coryclassic1989@gmail.com>
2022-07-03 11:44:15 -0400

    Time: 122 ms (19.48%), Space: 43.9 MB (86.48%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -10,16 +10,36 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    if(!list1 ) return list2
-    if(!list2) return list1
-    if(list1.val < list2.val){
-       list1.next = mergeTwoLists(list1.next , list2)
-        return list1
-       }
-    else{
-           list2.next = mergeTwoLists(list1 , list2.next)
-        return list2
-    }
 
-};
+//recursive solution
+// var mergeTwoLists = function(list1, list2) {
+//     if(!list1 ) return list2
+//     if(!list2) return list1
+//     if(list1.val < list2.val){
+//        list1.next = mergeTwoLists(list1.next , list2)
+//         return list1
+//        }
+//     else{
+//            list2.next = mergeTwoLists(list1 , list2.next)
+//         return list2
+//     }
+
+// };
+
+//itterative O(min(N,M)) time || O(1) space
+const mergeTwoLists = function(l1, l2) {
+    const head = new ListNode(null)
+    let curr = head;
+    while(l1 && l2){
+        if (l1.val < l2.val){
+            curr.next=l1;
+            l1 = l1.next
+        } else {
+            curr.next = l2;
+            l2 = l2.next
+        }
+        curr=curr.next
+    }
+    curr.next = l1 || l2;
+    return head.next;
+}
