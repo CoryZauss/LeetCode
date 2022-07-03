5989b868c36e958452e13355fb6461eada0a4dde
Cory Zauss <coryclassic1989@gmail.com>
2022-07-03 11:22:40 -0400

    Time: 125 ms (9.02%), Space: 44.1 MB (63.42%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.js
+++ 206-reverse-linked-list/206-reverse-linked-list.js
@@ -21,17 +21,32 @@
     // return newlist;
 //};
 
-//iterative o(n) time
+//itterative o(n) time
+// var reverseList = function(head) {
+//     let prev = null, curr = head, next = null;
+//     while(curr !== null) {
+//         //save next
+//         next = curr.next;
+//         //reverse
+//         curr.next = prev;
+//         //advance prev and curr
+//         prev = curr;
+//         curr = next;
+//     }
+//     return prev;
+// }
+
+// recursive
 var reverseList = function(head) {
-    let prev = null, curr = head, next = null;
-    while(curr !== null) {
-        //save next
-        next = curr.next;
-        //reverse
-        curr.next = prev;
-        //advance prev and curr
-        prev = curr;
-        curr = next;
+	// base case
+    if (head == null || head.next == null){
+        return head;
     }
-    return prev;
-}
+	// go all the way to the end
+    let reversedListHead = reverseList(head.next)
+	// add reverse myself
+    head.next.next = head;
+    head.next = null;
+	// go up
+    return reversedListHead
+};
