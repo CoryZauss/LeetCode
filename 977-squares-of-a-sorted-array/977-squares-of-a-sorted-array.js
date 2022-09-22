4952db31c3f122a716e5a3e53b83d7d6a1fbf9c8
Cory Zauss <coryclassic1989@gmail.com>
2022-09-22 19:32:28 -0400

    Time: 168 ms (51.84%), Space: 48.7 MB (59.11%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.js
@@ -2,29 +2,50 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    //pop last , square it, insert in new array at right place
-    let result = []
-    while(nums.length){
-        let pop = nums.pop()
-        pop = pop*pop
-        insertInPlace(result,pop)
-    }
-    return result
-};
 
-const insertInPlace = (array, num) => {
-    if (array.length === 0) return array.push(num)
-    if (num > array.at(-1)) return array.push(num)
+let sortedSquares = function(nums) {
+    let result = [], start = 0, end = nums.length-1;
     
-    for (let i  = array.length-1; i >= 0; i--) {
-        if (num > array[i]) {
-            array.splice(i+1, 0, num)
-            break;
-        }
-        if(i === 0) {
-            array.unshift(num)
-            break;
+    while(start <= end ){
+        if (nums[start] **2 > nums[end]**2){
+            result.push(nums[start]**2)
+            start++
+        } else {
+            result.push(nums[end]**2);
+            end--;
         }
     }
+    return result.reverse()
 }
+
+
+
+
+
+
+// var sortedSquares = function(nums) {
+//     //pop last , square it, insert in new array at right place
+//     let result = []
+//     while(nums.length){
+//         let pop = nums.pop()
+//         pop = pop*pop
+//         insertInPlace(result,pop)
+//     }
+//     return result
+// };
+
+// const insertInPlace = (array, num) => {
+//     if (array.length === 0) return array.push(num)
+//     if (num > array.at(-1)) return array.push(num)
+    
+//     for (let i  = array.length-1; i >= 0; i--) {
+//         if (num > array[i]) {
+//             array.splice(i+1, 0, num)
+//             break;
+//         }
+//         if(i === 0) {
+//             array.unshift(num)
+//             break;
+//         }
+//     }
+// }
