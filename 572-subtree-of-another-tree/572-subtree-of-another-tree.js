551ecfb4d6c09cd09a2d730bacac43a29c53bcf4
Cory Zauss <coryclassic1989@gmail.com>
2022-09-22 18:44:18 -0400

    Time: 164 ms (13.93%), Space: 49.3 MB (55.62%) - LeetHub

--- 572-subtree-of-another-tree/572-subtree-of-another-tree.js
+++ 572-subtree-of-another-tree/572-subtree-of-another-tree.js
@@ -11,17 +11,35 @@
  * @param {TreeNode} subRoot
  * @return {boolean}
  */
-var isSubtree = function(root, subRoot) {
+//dfs solution ->
+// var isSubtree = function(root, subRoot) {
   
-    if (!root) return !subRoot;
-    return isEqual(root, subRoot) 
-    || isSubtree(root.left, subRoot) 
-    || isSubtree(root.right, subRoot)
+//     if (!root) return !subRoot;
+//     return isEqual(root, subRoot) 
+//     || isSubtree(root.left, subRoot) 
+//     || isSubtree(root.right, subRoot)
     
-};
+// };
 
-const isEqual = (node1, node2) =>{
-    if(!node1 || !node2) return !node1 && !node2
-    if(node1.val !== node2.val ) return false;
-    return isEqual(node1.left, node2.left) && isEqual(node1.right, node2.right)
-}
+// const isEqual = (node1, node2) =>{
+//     if(!node1 || !node2) return !node1 && !node2
+//     if(node1.val !== node2.val ) return false;
+//     return isEqual(node1.left, node2.left) && isEqual(node1.right, node2.right)
+// }
+
+//hybrid solution ->
+const isSubtree = (root, subRoot) => {
+  const areEqual = (node1, node2) => {
+    if (!node1 || !node2) return !node1 && !node2;
+    if (node1.val !== node2.val) return false;
+    return areEqual(node1.left, node2.left) && areEqual(node1.right, node2.right);
+  }
+  const queue = [root];
+  while (queue.length) {
+    const node = queue.pop();
+    if (!node) continue;
+    if (areEqual(node, subRoot)) return true;
+    queue.push(node.right, node.left);
+  }
+  return false;
+};
