f6086f4fa25c5b26caba9abefc43daeacbf4168f
Cory Zauss <coryclassic1989@gmail.com>
2024-11-08 18:25:22 -0500

    Time: 15 ms (49.49%), Space: 16.7 MB (91.72%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,19 +6,28 @@ class Solution:
         d = {}
         d2 = {}
         
-        for i, char in enumerate(s):
-            if char not in d:
-                d[char] = 1
-            else:
-                d[char] += 1
+#         for i, char in enumerate(s):
+#             if char not in d:
+#                 d[char] = 1
+#             else:
+#                 d[char] += 1
                 
-            if t[i] not in d2:
-                d2[t[i]] = 1
-            else:
-                d2[t[i]] += 1
+#             if t[i] not in d2:
+#                 d2[t[i]] = 1
+#             else:
+#                 d2[t[i]] += 1
                 
-        if d == d2:
-            return True
-        else:
-            return False
+#         if d == d2:
+#             return True
+#         else:
+#             return False
         
+        
+        
+        for i in range(len(s)):
+            d[s[i]] = 1 + d.get(s[i], 0) # can .get and provide 0 as second param meaning if key value doesnt exist it will create it with 0 as value and then add 1
+            d2[t[i]] = 1 + d2.get(t[i], 0)
+        for c in d:
+            if d[c] != d2.get(c, 0):
+                return False
+        return True
