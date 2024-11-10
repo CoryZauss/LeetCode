ae8b84665a38dea3c1525940888ad6c5215cbe1d
Cory Zauss <coryclassic1989@gmail.com>
2024-11-09 19:20:57 -0500

    Time: 20 ms (80.10%), Space: 28.3 MB (46.42%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -2,30 +2,39 @@ class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         #create a window using 2 pointers 10 apart
         #slide it down, keep track of the 10 dig in a dict
-        #itterate on the counts
-       
-        if len(s) < 10:
-            return []
+        #itterate on the count
+        seen, res = set(), set()
+        
+        for l in range(len(s)-9):
+            cur = s[l:l+10]
+            if cur in seen:
+                res.add(cur)
+            seen.add(cur)
+        return list(res)
     
-        d ={}
+    
+#         if len(s) < 10:
+#             return []
+    
+#         d ={}
         
-        l=0
-        r=10
+#         l=0
+#         r=10
         
-        while r <= len(s):
-            cur = s[l:r]
+#         while r <= len(s):
+#             cur = s[l:r]
         
-            if cur not in d:
-                d[cur] = 1
-            else:
-                d[cur] += 1
-            l+=1
-            r+=1
+#             if cur not in d:
+#                 d[cur] = 1
+#             else:
+#                 d[cur] += 1
+#             l+=1
+#             r+=1
         
-        res = []
-        for key, value in d.items():
-            if value > 1:
-                res.append(key)
-        print(d)
-        print(res)        
-        return res
+#         res = []
+#         for key, value in d.items():
+#             if value > 1:
+#                 res.append(key)
+#         print(d)
+#         print(res)        
+#         return res
