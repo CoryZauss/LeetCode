30c7d6ac525a0ce5b97167eee4376ef8e46cdc92
Cory Zauss <coryclassic1989@gmail.com>
2024-11-15 20:40:12 -0500

    Time: 0 ms (100.00%), Space: 17.2 MB (31.65%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.py
@@ -2,29 +2,32 @@ class Solution:
     
 
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        
-        def binary_search(arr, target):
-            if len(arr) == 1 and arr[0] == target: 
-                return 0
-                
-            l,r= 0, len(arr)-1
-            for i in range(len(arr)):
-                mid = (r+l)//2
-                if arr[mid] == target:
-                    return mid
-                elif arr[mid] > target:
-                    r = mid -1
-                else:
-                    l = mid +1
-            return -1
-    
-        arr = matrix
-        for i in range(len(arr)):
-            x = binary_search(arr[i],target)
-            if x > -1:
+        ROWS,COLS = len(matrix), len(matrix[0])
+        top, bot = 0, ROWS -1
+        while top <= bot:
+            mid = (top + bot) // 2
+            if target > matrix[mid][-1]: ##! looks at right most value
+                top = mid + 1
+            elif target < matrix[mid][0]:
+                bot = mid - 1
+            else: #! target is in this row
+                break
+
+        if not (top <=bot):#! no rows have target
+            return False
+
+        mid = (top + bot) // 2
+        l,r = 0, COLS -1
+        while l <= r:
+            m = (l + r) // 2
+            if target > matrix[mid][m]:
+                l = m+1
+            elif target < matrix[mid][m]:
+                r = m -1
+            else:
                 return True
-            
         return False
+
 
 
     
